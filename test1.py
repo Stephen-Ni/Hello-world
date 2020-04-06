9c9d1b776fce2ab3c9ff094811c82f83ae3e1eef
Stephen-Ni <57473502+Stephen-Ni@users.noreply.github.com>
2020-04-06 13:39:30 +0800

    小甲鱼题目
    test

--- test1.py
+++ test1.py
@@ -1,8 +1,9 @@
-print('Hello World!')
-input=('put into a number:')
-guess=int(input)
+print('----------我爱鱼c工作室---------')
+temp=input('不妨猜一下小鲫鱼现在心里想的是那个数:')
+guess=int(temp)
 if guess==8:
-    print('game over')
+    print('我草，你是小酒肚子里的蛔虫啊吗')
 else:
-    print('Try again!')
+    print('别猜啦，小甲鱼现在心里想的是8')
+print('游戏结束，不玩啦！')
 
